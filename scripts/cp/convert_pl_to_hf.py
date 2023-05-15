1db9ef08f8441a05a673907a4c5e63899620d314
ShinoharaHare <anna.shiker1822@gmail.com>
2023-05-16 04:53:47 +0800

    perf: 優化轉換腳本

--- scripts/cp/convert_pl_to_hf.py
+++ scripts/cp/convert_pl_to_hf.py
@@ -1,17 +1,30 @@
-from typing import Dict, Optional, cast
+import functools
+from typing import Any, Dict, Optional, cast
 
 import fire
 import torch
+from accelerate import init_empty_weights
 from transformers import (AutoConfig, AutoModelForCausalLM, AutoTokenizer,
                           PreTrainedModel)
-from transformers.modeling_utils import no_init_weights
 
 from taide_cp.utils.deepspeed import get_state_dict_from_zero_checkpoint
 
 
+def rgetattr(obj: Any, attr: str):
+    return functools.reduce(getattr, [obj] + attr.split('.'))
+
+def rsetattr(obj: Any, attr: str, val: Any):
+    pre, _, post = attr.rpartition('.')
+    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+
 def patch_state_dict(state_dict: Dict[str, torch.Tensor]):
-    for k in list(state_dict.keys()):
-        state_dict[k.removeprefix('_forward_module.model.')] = state_dict.pop(k)
+    return {k.removeprefix('_forward_module.model.'): v for k, v in state_dict.items()}
+
+def load_state_dict(model: PreTrainedModel, state_dict: Dict[str, torch.Tensor]):
+    for k, v in state_dict.items():
+        rsetattr(model, k, torch.nn.Parameter(v))
+    model.tie_weights()
+    return model
 
 def main(
     config_path: str,
@@ -21,22 +34,18 @@ def main(
 ):
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path or config_path, use_fast=True)
     config = AutoConfig.from_pretrained(config_path)
-    with no_init_weights():
-        torch.set_default_dtype(torch.half)
+
+    with init_empty_weights():
         model = AutoModelForCausalLM.from_config(config)
         model = cast(PreTrainedModel, model)
-        torch.set_default_dtype(torch.float)
 
     if tokenizer_path is not None and model.config.vocab_size != len(tokenizer):
         model.resize_token_embeddings(len(tokenizer))
 
-    state_dict = torch.load(checkpoint_path, 'cpu')
-    patch_state_dict(state_dict)
-    incompatible_keys = model.load_state_dict(state_dict, strict=False)
-    missing_keys = set(incompatible_keys.missing_keys) - set(model._keys_to_ignore_on_load_missing or [])
-    assert not missing_keys
-    model.tie_weights()
-
+    state_dict = get_state_dict_from_zero_checkpoint(checkpoint_path, dtype=torch.half)
+    state_dict = patch_state_dict(state_dict)
+    model = load_state_dict(model, state_dict)
+    
     model.save_pretrained(output_path, max_shard_size='1000GB', safe_serialization=True)
     tokenizer.save_pretrained(output_path)
 
