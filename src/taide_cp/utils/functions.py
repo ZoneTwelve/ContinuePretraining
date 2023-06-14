c64e5e80ec9dfb491c23087f4ad61b4f853489f4
ShinoharaHare <anna.shiker1822@gmail.com>
2023-06-15 07:51:22 +0800

    chore: 新增 token 計算腳本

--- src/taide_cp/utils/functions.py
+++ src/taide_cp/utils/functions.py
@@ -1,8 +1,10 @@
 import functools
 import json
-from typing import Any
+import logging
+from contextlib import ContextDecorator
+from typing import Any, Dict
 
-__all__ = ['read_json', 'write_json', 'rgetattr', 'rsetattr']
+__all__ = ['read_json', 'write_json', 'rgetattr', 'rsetattr', 'DatasetsContextManager']
 
 def read_json(path: str) -> Any:
     with open(path, 'r', encoding='utf-8') as f:
@@ -18,3 +20,57 @@ def rgetattr(obj: Any, attr: str):
 def rsetattr(obj: Any, attr: str, val: Any):
     pre, _, post = attr.rpartition('.')
     return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+
+class DatasetsContextManager(ContextDecorator):
+    @staticmethod
+    def set_caching_enabled(boolean: bool):
+        import datasets
+
+        if boolean:
+            datasets.enable_caching()
+        else:
+            datasets.disable_caching()
+
+    @staticmethod
+    def set_progress_bar_enabled(boolean: bool):
+        import datasets
+
+        if boolean:
+            datasets.enable_progress_bar()
+        else:
+            datasets.disable_progress_bar()
+
+    @classmethod
+    def set_state(cls, state: Dict[str, Any]):
+        import datasets
+        cls.set_caching_enabled(state['caching'])
+        cls.set_progress_bar_enabled(state['progress_bar'])
+        datasets.logging.set_verbosity(state['verbosity'])
+
+    def __init__(
+        self,
+        caching: bool = False,
+        progress_bar: bool = False,
+        verbosity: int = logging.ERROR,
+    ) -> None:
+        super().__init__()
+
+        import datasets
+        
+        self.old_state = {
+            'caching': datasets.is_caching_enabled(),
+            'progress_bar': datasets.is_progress_bar_enabled(),
+            'verbosity': datasets.logging.get_verbosity(),
+        }
+
+        self.new_state = {
+            'caching': caching,
+            'progress_bar': progress_bar,
+            'verbosity': verbosity,
+        }
+    
+    def __enter__(self):
+        self.set_state(self.new_state)
+    
+    def __exit__(self, __exc_type, __exc_value, __traceback):
+        self.set_state(self.old_state)
