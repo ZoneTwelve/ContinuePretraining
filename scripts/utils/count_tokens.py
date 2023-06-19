b0237b809249997c52ddcc4bd37e0da36a7889ce
ShinoharaHare <anna.shiker1822@gmail.com>
2023-06-19 22:59:32 +0800

    chore: 新增計算 token 腳本

--- scripts/utils/count_tokens.py
+++ scripts/utils/count_tokens.py
@@ -38,7 +38,15 @@ def main(
     disable_caching()
 
     dataset = load_datasets(data_path)
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast=False)
+    tokenizer_init_kwargs = dict(
+        use_fast=False,
+        model_max_length=None,
+    )
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, **tokenizer_init_kwargs)
+    except ValueError:
+        tokenizer_init_kwargs['use_fast'] = True
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, **tokenizer_init_kwargs)
 
     dataset = dataset.map(
         tokenize,
