992a7b244e652d1976bd880a91772e2ae92cf0c1
ShinoharaHare <anna.shiker1822@gmail.com>
2023-07-12 02:34:09 +0800

    fix: 改回使用 register

--- src/taide_cp/models/auto.py
+++ src/taide_cp/models/auto.py
@@ -1,58 +1,17 @@
 from os import PathLike
 
-from transformers import (AutoConfig, AutoModelForCausalLM, AutoTokenizer,
-                          PretrainedConfig, PreTrainedModel)
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 from transformers.tokenization_utils import PreTrainedTokenizer
 from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 
+from ..models.mpt import MPTConfig, MPTForCausalLM
+
 __all__ = ['AutoConfig', 'AutoModelForCausalLM', 'AutoTokenizer']
 
-class AutoConfig(AutoConfig):
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path: str | PathLike, **kwargs) -> PretrainedConfig:
-        config_dict, _ = PretrainedConfig.get_config_dict(pretrained_model_name_or_path, **kwargs)
 
-        if config_dict.pop('model_type', None) == 'mpt':
-            from ..models.mpt import MPTConfig
+AutoConfig.register('mpt', MPTConfig)
+AutoModelForCausalLM.register(MPTConfig, MPTForCausalLM)
 
-            kwargs.pop('trust_remote_code', None)
-            return MPTConfig.from_pretrained(pretrained_model_name_or_path, **kwargs)
-
-        return super().from_pretrained(pretrained_model_name_or_path, **kwargs)
-
-
-class AutoModelForCausalLM(AutoModelForCausalLM):
-    @classmethod
-    def from_config(cls, config: PretrainedConfig, **kwargs):
-        if config.model_type == 'mpt':
-            from ..models.mpt import MPTForCausalLM
-            kwargs.pop('trust_remote_code', None)
-            return MPTForCausalLM._from_config(config, **kwargs)
-
-        return super().from_config(config, **kwargs)
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path: str | PathLike, *model_args, **kwargs) -> PreTrainedModel:
-        config = kwargs.pop('config', None)
-        trust_remote_code = kwargs.pop('trust_remote_code', None)
-
-        org_config = config
-        if not isinstance(config, PretrainedConfig):
-            config, kwargs = AutoConfig.from_pretrained(
-                pretrained_model_name_or_path,
-                return_unused_kwargs=True,
-                trust_remote_code=trust_remote_code,
-                **kwargs,
-            )
-
-        if config.model_type == 'mpt':
-            from ..models.mpt import MPTForCausalLM
-
-            kwargs.pop('trust_remote_code', None)
-            return MPTForCausalLM.from_pretrained(pretrained_model_name_or_path, config=config, *model_args, **kwargs)
-
-        return super().from_pretrained(pretrained_model_name_or_path, config=org_config, *model_args, **kwargs)
-    
 
 class AutoTokenizer(AutoTokenizer):
     @classmethod
