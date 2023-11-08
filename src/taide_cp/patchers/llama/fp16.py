a62c9449f35edb0814c6c4dbe66cc2470e1676f0
ShinoharaHare <anna.shiker1822@gmail.com>
2023-11-08 10:53:04 +0800

    fix: `LLaMAFP16Patcher` 改用可變參數

--- src/taide_cp/patchers/llama/fp16.py
+++ src/taide_cp/patchers/llama/fp16.py
@@ -15,24 +15,8 @@ def _clamp_fp16(x: torch.Tensor):
     return x
 
 
-def _decoder_layer_forward(
-    self: LlamaDecoderLayer,
-    hidden_states: torch.Tensor,
-    attention_mask: Optional[torch.Tensor] = None,
-    position_ids: Optional[torch.LongTensor] = None,
-    past_key_value: Optional[Tuple[torch.Tensor]] = None,
-    output_attentions: Optional[bool] = False,
-    use_cache: Optional[bool] = False,
-) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
-    outputs = LlamaDecoderLayer.forward(
-        self,
-        hidden_states=hidden_states,
-        attention_mask=attention_mask,
-        position_ids=position_ids,
-        past_key_value=past_key_value,
-        output_attentions=output_attentions,
-        use_cache=use_cache,
-    )
+def _decoder_layer_forward(self: LlamaDecoderLayer, *args, **kwargs):
+    outputs = LlamaDecoderLayer.forward(self, *args, **kwargs)
     outputs = (_clamp_fp16(outputs[0]), *outputs[1:])
     return outputs
 
